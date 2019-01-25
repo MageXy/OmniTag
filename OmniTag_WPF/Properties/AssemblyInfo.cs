4d0fb9ab0cb42f6210ae36324ecbd2238cb952c8
MageXy <nikgieske@gmail.com>
2019-01-25 17:10:09 -0600

    Migrate to GitHub

--- OmniTag_WPF/Properties/AssemblyInfo.cs
+++ OmniTag_WPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OmniTag_v2")]
+[assembly: AssemblyTitle("OmniTag")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OmniTag_v2")]
+[assembly: AssemblyProduct("OmniTag")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
